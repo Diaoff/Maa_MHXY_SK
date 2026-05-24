9db38df18b09d096d570f982cf37e187d26590e7
gitlihang <2664129943@qq.com>
2026-05-25 00:16:27 +0800

    perf:优化returnOCR函数

--- agent/custom/action/returnOCR.py
+++ agent/custom/action/returnOCR.py
@@ -13,10 +13,13 @@ class ReturnOCR(CustomAction):
             "action_key": "Click",
             "recognition_name": "识别输出测试",
             "return_text": "输出的描述"
+            "click_target": []  # 点击坐标，格式为[x1, y1, x2, y2]，仅在action_key为Click时使用
         }
         action_key: 动作名称，用于判断动作类型，如Click、Move等
         recognition_name: task任务名称,用于指定识别任务名称，返回该节点的结果。
         return_text: 输出的描述，用于指定返回的描述
+        click_target: 点击坐标，格式为[x1, y1, x2, y2]，仅在action_key为Click时使用。如果不提供click_target，则默认点击识别结果的中心位置。
+
     """
     def run(
         self,
@@ -32,6 +35,7 @@ class ReturnOCR(CustomAction):
         action_key = argv_dict.get("action_key", "")
         recognition_name = argv_dict.get("recognition_name", "")
         return_text = argv_dict.get("return_text", "")
+        click_target = argv_dict.get("click_target", [])
 
         # 获取ocr识别结果数据
         image = context.tasker.controller.post_screencap().wait().get()
@@ -46,8 +50,15 @@ class ReturnOCR(CustomAction):
             logger.info(f"{return_text}: {best_result.text}")
             # 根据action_key执行不同的动作
             if action_key == "Click":
+                # 点击传入参数中的坐标位置
+                if click_target:
+                    box = click_target
+                    center_x = box[0] + box[2] // 2
+                    center_y = box[1] + box[3] // 2
+                    logger.debug(f"点击位置: ({center_x}, {center_y})")
+                    context.tasker.controller.post_click(center_x, center_y).wait()
                 # 点击最佳识别结果的中心位置
-                if best_result:
+                elif best_result:
                     box = best_result.box
                     center_x = box[0] + box[2] // 2
                     center_y = box[1] + box[3] // 2
